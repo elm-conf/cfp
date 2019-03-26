12b7300ab2d3da78d7f0da630577d29369daeada
Brian Hicks <brian@brianthicks.com>
2019-03-26 05:15:41 -0500

    start nix

---
+++ shell.nix
@@ -0,0 +1,14 @@
+with import (builtins.fetchTarball rec {
+  # grab a hash from here: https://nixos.org/channels/
+  name = "nixpkgs-darwin-18.09pre153971.2d656a97297";
+  url = "https://github.com/nixos/nixpkgs/archive/2d656a9729739e8ca5486058a76669d0ea4442df.tar.gz";
+  # Hash obtained using `nix-prefetch-url --unpack <url>`
+  sha256 = "00aqc7am069vgmlg60v55hjahv7kkfkn9z6lwirqr1wzpcgll3v1";
+}) {};
+
+stdenv.mkDerivation {
+  name = "cfp";
+  buildInputs = [
+    git
+  ];
+}
