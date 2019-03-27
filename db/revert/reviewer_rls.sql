416f11eaa484e2c24a3f329f88fea7e13a673918
Brian Hicks <brian@brianthicks.com>
2019-03-26 23:01:34 -0500

    fix rls reversion script

--- db/revert/reviewer_rls.sql
+++ db/revert/reviewer_rls.sql
@@ -2,8 +2,8 @@
 
 BEGIN;
 
-DROP POLICY update_reviewer;
-DROP POLICY select_reviewer;
+DROP POLICY update_reviewer ON cfp_public.reviewer;
+DROP POLICY select_reviewer ON cfp_public.reviewer;
 ALTER TABLE cfp_public.reviewer DISABLE ROW LEVEL SECURITY;
 
 COMMIT;
