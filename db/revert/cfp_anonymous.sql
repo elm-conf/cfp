086b421ac91754e6789502733af339b66a414acc
Brian Hicks <brian@brianthicks.com>
2019-03-26 22:26:27 -0500

    drop more things so revert scripts work

--- db/revert/cfp_anonymous.sql
+++ db/revert/cfp_anonymous.sql
@@ -2,6 +2,11 @@
 
 BEGIN;
 
+REVOKE ALL PRIVILEGES ON ALL TABLES IN SCHEMA cfp_public FROM cfp_anonymous;
+REVOKE ALL PRIVILEGES ON ALL FUNCTIONS IN SCHEMA cfp_public FROM cfp_anonymous;
+REVOKE ALL PRIVILEGES ON SCHEMA cfp_public FROM cfp_anonymous;
+
+REVOKE cfp_anonymous FROM postgraphile;
 DROP ROLE cfp_anonymous;
 
 COMMIT;
--- db/revert/cfp_reviewer.sql
+++ db/revert/cfp_reviewer.sql
@@ -2,6 +2,10 @@
 
 BEGIN;
 
+REVOKE ALL PRIVILEGES ON ALL TABLES IN SCHEMA cfp_public FROM cfp_reviewer;
+REVOKE ALL PRIVILEGES ON SCHEMA cfp_public FROM cfp_reviewer;
+
+REVOKE cfp_reviewer FROM postgraphile;
 DROP ROLE cfp_reviewer;
 
 COMMIT;
