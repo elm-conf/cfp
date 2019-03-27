400469fe8f3949db6dca9d781dc09cee9a230d1a
Brian Hicks <brian@brianthicks.com>
2019-03-26 22:57:21 -0500

    add new security config to postgraphile

--- index.js
+++ index.js
@@ -6,12 +6,20 @@ const server = postgraphile(
   process.env.DATABASE_URL || "postgres://postgraphile:dev@localhost/cfp",
   process.env.SCHEMA_NAME || "cfp_public",
   {
-    watchPg: true,
     appendPlugins: [
       require("@graphile-contrib/pg-simplify-inflector")
     ],
+
+    // security
+    defaultRole: 'cfp_anonymous',
+    secret: process.env.JWT_SECRET || 'super bad dev secret',
+    token: 'cfp_public.jwt_token',
+
+    // deveopment
+    watchPg: true,
     graphiql: graphiql,
     enhanceGraphiql: graphiql,
+
     // TODO: errors. There are nice options here: https://www.graphile.org/postgraphile/usage-library/
   }
 );
