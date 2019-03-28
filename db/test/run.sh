85d57e2e687c5c8ce4b980116ea2d7c7b52e1456
Brian Hicks <brian@brianthicks.com>
2019-03-28 15:32:16 -0500

    slightly fancier than that, actually

--- db/test/run.sh
+++ db/test/run.sh
@@ -3,7 +3,13 @@ set -euo pipefail
 
 TEST_DIR="$(dirname ${0:-})"
 
+EXIT=0
+
 for testname in $TEST_DIR/test_*.sql; do
-    echo "$testname"
+    echo "========== $(basename $testname)"
     psql -d cfp -c 'BEGIN;' -c "\i $testname;" -c 'ROLLBACK;'
+    if test "$?" -ne 0; then EXIT=1; fi
+    echo
 done
+
+exit "$EXIT"
