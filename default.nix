360a349cbff86e44c91d3b1c641d0bbf96ea9d86
Brian Hicks <brian@brianthicks.com>
2019-04-02 16:58:36 -0500

    compose all the derivations together

---
+++ default.nix
@@ -0,0 +1,25 @@
+{ pkgs ? import <nixpkgs> {
+    inherit system;
+  }
+, system ? builtins.currentSystem }:
+
+let
+  db = pkgs.callPackage ./db { };
+  backend = (pkgs.callPackage ./backend { }).package;
+in
+  pkgs.stdenv.mkDerivation {
+    name = "cfp";
+    version = "1.0.0";
+
+    src = ./.;
+
+    phases = [ "installPhase" ];
+
+    installPhase = ''
+    mkdir -p $out/bin
+
+    ln -s ${db}/bin/cfp-db-init $out/bin/cfp-db-init
+    ln -s ${db}/bin/cfp-sqitch $out/bin/cfp-sqitch
+    ln -s ${backend}/bin/cfp-backend $out/bin/cfp-backend
+    '';
+  }
